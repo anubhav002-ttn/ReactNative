d0427c36f62b706037e64767509fb2411d2c54f8
Anubhav <anubhav.rajput@tothenew.com>
2023-03-14 08:42:21 +0530

    assignment redux

--- AwesomeProject/Redux/Reducers/IncDec.js
+++ AwesomeProject/Redux/Reducers/IncDec.js
@@ -4,33 +4,29 @@ const initialState = [
         name: 'Orange',
         Price: '2.5',
         image: require('../../assets/Images/orange.webp'),
-        count: 0
+        count: 5
     },
     {
         id: 2,
         name: 'Apple',
         Price: '3',
         image: require('../../assets/Images/apple.jpg'),
-        count: 0
+        count: 5
     },
     {
         id: 3,
         name: 'Strawberry',
         Price: '3',
         image: require('../../assets/Images/strawberry.jpg'),
-        count: 0
+        count: 5
     }
 ]
-
-// const initialstate ={
-//     ptoducts:
-// }
 
 const counterReducer = (state = initialState, action) => {
     switch (action.type) {
         case "INCREMENT":
-             console.log('action',action)
-             return state.map((item) => {
+            console.log('action', action.id)
+            return state.map((item) => {
                 if (item.id !== action.id) {
                     console.log('state', item)
                     return item;
@@ -38,20 +34,24 @@ const counterReducer = (state = initialState, action) => {
                 console.log('state1', item)
                 return {
                     ...item,
-                    count: item.count+1
+                    count: item.count + 1
                 };
             })
-        console.log('state2',state)
-        return state;
         case "DECREMENT":
             return state.map((item) => {
-                if (item.id !== action.id) {
-                    return item;
+                if (item[action.id-1].count > 0) {
+                    if (item.id !== action.id) {
+                        return item;
+                    }
+                    // if(item.count>0){
+                    return {
+                        ...item,
+                        count: item.count - 1
+                    };
                 }
-                return {
-                    ...item,
-                    count: item.count - 1
-                };
+                // else if(item.id !== action.id) {
+                //     return item;
+                // }
             })
         //   case "DELETE_ITEM":
         //     return (state = 0);
